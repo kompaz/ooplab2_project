037540e0454d410fb3ce4f02a4554298a1eef4f5
sinankompas <sinankompas@gmail.com>
2022-03-27 01:15:55 +0300

    kullanıcı adı ve şifre tanımlandı admin/user belirtildi, ekran açıldığında kullanıcı girişi yapıldı, enter tuşuna basılınca giriş yapıldı, taba basılıncı bi alta geçildi, giriş yapıldıktan sonra menü sayfası açıldı

--- ooplab/ooplab/Form1.cs
+++ ooplab/ooplab/Form1.cs
@@ -12,10 +12,47 @@ namespace ooplab
 {
     public partial class Form1 : Form
     {
+        private List<user> userList = new List<user>();
         public Form1()
         {
             InitializeComponent();
+            userList.Add(new user("admin", "admin", true));
+            userList.Add(new user("user", "user", false));
         }
 
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i=0;i<userList.Count;i++)
+            {
+                if(userList[i].Username == textBox1.Text && userList[i].Password == textBox2.Text)
+                {
+                    mainPage menu = new mainPage();
+                    //menu.Show();
+                    this.Hide();
+                    menu.ShowDialog();
+                    this.Close();
+                }
+            }
+        }
     }
 }
